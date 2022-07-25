5f3592ff11e207f93aab01faa06fd1888d47d805
kubaadamczyklogin <poczta@kubaadamczyk.pl>
2022-07-25 07:39:53 +0200

    Zmniejszenie opisów mikro aplikacji.

--- src/App.js
+++ src/App.js
@@ -23,6 +23,10 @@ const CustomSection = styled(Box)`
     padding: 25px;
   }
 
+  h1 {
+    color: white;
+  }
+
   h3 {
     margin: 15px 0 10px;
   }
@@ -46,7 +50,7 @@ export default function App() {
       <CssBaseline />
       <Container>
         <CustomSection>
-          <Typography variant="h4" component="h2">
+          <Typography variant="h4" component="h1">
             Kuba Adamczyk
           </Typography>
         </CustomSection>
@@ -58,9 +62,9 @@ export default function App() {
             <Box>
               <Typography component="p">
                 Obecnie pracuje w spółce Oponeo.PL S.A. Zajmuje się pracami
-                frontendowymi w zespole rozwijającym największy polski
-                internetowy sklep rowerowy CentrumRowerowe.pl. Sklep zbudowany
-                jest na klasycznym wzorcu MVC, frontend oparty o jQuery i Less.
+                frontendowymi w zespole rozwijającym internetowy sklep
+                CentrumRowerowe.pl. Aplikacja zbudowana jest na klasycznym
+                wzorcu MVC, frontend oparty o jQuery i Less.
               </Typography>
               <Typography component="p">
                 Czasem biorę udział w powiązanych projektach np:{" "}
@@ -86,36 +90,40 @@ export default function App() {
               <Link href="https://kursreacta.pl/">kursreacta.pl</Link> i piszę
               drobne aplikacje.
             </Typography>
+
             <Divider />
+
             <Typography variant="h5" component="h3">
-              Music Player
+              Pomodoro
             </Typography>
-            <Typography component="p">
-              Prosty audio player, pobierający utwory muzyczne z zewnętrznego
-              api. Wykonałem podczas kursu{" "}
-              <Link href="https://kursreacta.pl/">
-                "Kurs Reacta - Szybki start"
-              </Link>
-              .
-            </Typography>
+            <Typography component="p">Timer pomodoro z listą zadań.</Typography>
             <Button
               variant="outlined"
-              href="https://music-player-nine-self.vercel.app/"
+              href="https://pomodoro-ecru-one.vercel.app/"
             >
               Sprawdź
             </Button>
+
             <Divider />
+
+            <Typography variant="h5" component="h3">
+              Dojazdy PKP
+            </Typography>
+            <Typography component="p">
+              Aplikacja na telefon ułatwiająca moje dojazdy pociągiem do pracy.
+            </Typography>
+            <Button variant="outlined" href="https://trains.vercel.app/">
+              Sprawdź
+            </Button>
+
+            <Divider />
+
             <Typography variant="h5" component="h3">
               Losowy cytat
             </Typography>
             <Typography component="p">
-              Aplikację wykonałem dla mojej żony, uczącej się języka
-              angielskiego. Reactowa apka pobiera losowy cytat z serwera GraphQl
-              przy pomocy Apollo Client. Jest owocem kursu{" "}
-              <Link href="https://graphqlmastery.pl/">
-                GraphQL Mastery - Szybki Start
-              </Link>
-              .
+              Reactowa apka pobiera losowy cytat z serwera GraphQl przy pomocy
+              Apollo Client.
             </Typography>
             <Button
               variant="outlined"
@@ -123,40 +131,19 @@ export default function App() {
             >
               Sprawdź
             </Button>
+
             <Divider />
+
             <Typography variant="h5" component="h3">
-              Dojazdy PKP
+              Music Player
             </Typography>
             <Typography component="p">
-              Dojeżdżam do pracy z Torunia do Bydgoszczy rowerem i pociągiem.
-              Napisałem więc webową aplikacje na telefon - pomagającą wyszukać
-              optymalne połączenie. Po wybraniu pociągu można zobaczyć ile czasu
-              przepracuję, o której powinienem wyjść z firmy, o której będę w
-              domu itp. Umożliwia również skorygowanie czasu pracy o np
-              spóźnienie pociągu lub gdy zamierzam coś załatwić w drodze
-              powrotnej. Dane o rozkładzie jazdy oraz czasy dojazdu rowerem na
-              stacje - przechowuje na serwerze Apollo GraphQl, który sam
-              napisałem.
-            </Typography>
-            <Button variant="outlined" href="https://trains.vercel.app/">
-              Sprawdź
-            </Button>
-            <Divider />
-            <Typography variant="h5" component="h3">
-              Pomodoro (w budowie)
-            </Typography>
-            <Typography component="p">
-              Timer pomodoro do poprawy skuteczności pracy. Posiada również
-              panel do dodawania i edytowania zadań. Wykonany tak by
-              sygnalizował ukończenie zadania nawet gdy działa w tle (na
-              nieaktywnej karcie przeglądarki). Stylowanie z użyciem biblioteki
-              Material Ui. Inspirowany timerem pomodoro z kursu{" "}
-              <Link href="https://kursreacta.pl/">kursreacta.pl</Link>, jednak
-              wprowadziłem w nim dużo własnych modyfikacji.
+              Prosty audio player, pobierający utwory muzyczne z zewnętrznego
+              api.
             </Typography>
             <Button
               variant="outlined"
-              href="https://pomodoro-g3a5unkb2-kubaadamczyklogin.vercel.app/"
+              href="https://music-player-nine-self.vercel.app/"
             >
               Sprawdź
             </Button>
